8daebab7d189865ca586ef69ce137fd686e12037
anuj26298 <kumar26298anuj@gmail.com>
2021-06-15 22:21:43 +0530

    ADDED code for minimum fare, testcase passed

--- src/main/java/com/cabinvoiceservice/CabInvoiceService.java
+++ src/main/java/com/cabinvoiceservice/CabInvoiceService.java
@@ -5,6 +5,9 @@ public class CabInvoiceService {
     private static final int COST_PER_MINUTE = 1;
     private static final double MINIMUM_FARE = 5;
     public double calculateFare(double distance, int time) {
-        return (MINIMUM_COST_PER_KM * distance) + (COST_PER_MINUTE * time);
+        double fare = (MINIMUM_COST_PER_KM * distance) + (COST_PER_MINUTE * time);
+        if (fare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        return  fare;
     }
 }
--- src/test/java/com/cabinvoiceservice/CabInvoiceServiceTest.java
+++ src/test/java/com/cabinvoiceservice/CabInvoiceServiceTest.java
@@ -11,4 +11,11 @@ public class CabInvoiceServiceTest {
         double fare = cabInvoiceService.calculateFare(10,20);
         Assert.assertEquals(120, fare, 0);
     }
+
+    @Test
+    public void givenDistaceAndTime_WhenFareIsLessThenMinumumFare_ReturnMinimumFare() {
+        CabInvoiceService cabInvoiceService = new CabInvoiceService();
+        double fare = cabInvoiceService.calculateFare(0.2, 2);
+        Assert.assertEquals(5,fare,0);
+    }
 }
